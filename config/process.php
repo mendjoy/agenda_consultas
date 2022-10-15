31663d5c1877f7ced41b6814251062ba1e3a70c0
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-15 14:51:29 -0300

    pagina para mostrar contato isolado

--- config/process.php
+++ config/process.php
@@ -5,12 +5,34 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id; 
+    if(!empty($_GET)){
+        $id = $_GET["id"];
+    }
 
-    $query = "SELECT * FROM contacts";
+    //retorna apenas um dado 
+    if(!empty($id)) {
 
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contacts = $stmt->fetchAll(); //recebe todos os dados por meio da PDO 
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch();
+      } else {
+  
+        // Retorna todos os contatos
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+  
+      }
+  
+    
+  
+    // FECHAR CONEXÃO
+    $conn = null;
+   
 
 ?>
